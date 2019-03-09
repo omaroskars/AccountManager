e7c65fef062caa80eb1d2f4574ca3a1f147f1f54
Omar Oskarsson <omaroskars15@gmail.com>
2019-03-09 23:51:16 +0000

    disable csrf

--- src/main/java/com/account/manager/ManagerApplication.java
+++ src/main/java/com/account/manager/ManagerApplication.java
@@ -2,12 +2,18 @@ package com.account.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class ManagerApplication {
+public class ManagerApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable(); // No need for CSRF in this example
+	}
 }
