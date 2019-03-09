0226ee9cb7cce7246f055383833306c4d8c42bf2
Ómar Óskarsson <omar.oskarsson@advania.is>
2019-03-09 18:26:29 +0000

    Generate data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into users
+values(1, 'Omar', 'Oskarsson');
+insert into users
+values(2, 'Leifur', 'Arason');
+insert into users
+values(3, 'Þorsteinn', 'Gunnarsson');
+insert into users
+values(4, 'Hekla', 'Pétursdóttir');
