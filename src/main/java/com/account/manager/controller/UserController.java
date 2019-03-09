3cc9d5c100dc8516b1df3729e0a7c5b1844eab7d
Ómar Óskarsson <omar.oskarsson@advania.is>
2019-03-09 18:44:16 +0000

    get user by id

--- src/main/java/com/account/manager/controller/UserController.java
+++ src/main/java/com/account/manager/controller/UserController.java
@@ -4,22 +4,34 @@ import com.account.manager.repository.UserRepository;
 import com.account.manager.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/users/")
 public class UserController {
   @Autowired
   private UserRepository userRepository;
 
-  @GetMapping(value = "/users")
+  // Get all users
+  @GetMapping(value = "")
   public List<User> getUsers() {
     return userRepository.findAll();
+  }
+
+  // Get user by id.
+  @GetMapping(value = "{id}")
+  public ResponseEntity<User> getUser(@PathVariable Long id) throws Exception {
+    User user = userRepository.findById(id).orElseThrow(() -> new Exception("User not found on :: " + id));
+
+    return ResponseEntity.ok().body(user);
+
   }
 
 }
